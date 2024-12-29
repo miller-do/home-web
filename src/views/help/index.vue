de26f59acb5ecc097eef72d33a3370f8aa365670
Your Name <you@example.com>
2024-12-29 19:32:35 +0800

    feat:添加vanta背景动画

--- src/views/help/index.vue
+++ src/views/help/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>help</div>
+  <div>
+    <!-- <v-vanta></v-vanta> -->
+    <v-vanta
+      ref="vantaInstance"
+      vantaId="vanta-bg"
+      :effect="effect"
+      :options="options"
+    >
+      <slot />
+    </v-vanta>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import VVanta from 'vue-vanta-revamp'
 
-<style lang="scss" scoped></style>
+const options = {
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x3ff7ff,
+  shininess: 85.0,
+  waveHeight: 6.0,
+  waveSpeed: 1,
+  zoom: 0.75,
+}
+
+const effect = 'globe'
+</script>
+
+<style lang="scss" scoped>
+#vanta-bg {
+  z-index: -1000;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
